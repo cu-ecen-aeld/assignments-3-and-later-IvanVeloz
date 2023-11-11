7977e36b310650a7486d8a02a871923c428bf5dc
Ivan Veloz <devel@ivanveloz.com>
2023-11-11 02:21:04 +0000

    Added username validation

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -14,9 +14,25 @@
 */
 void test_validate_my_username()
 {
+
     /**
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *hardUN = my_username();
+    char *confUN = malloc_username_from_conf_file();
+    
+    /*
+    bool namesMatch = true;
+
+    for(int i=0; namesMatch; i++) {
+        if(*(hardUN+i) && *(confUN+i)) // if both are not NULL
+            namesMatch = *(hardUN+i) == *(confUN+i);
+        else
+            namesMatch = false;
+    } 
+    */
+
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(hardUN, confUN, "The username on username.txt does not match my_username()");
+
 }
