7136c160fee73262fd9e9e2fe97c5486dfe12be2
Ivan Veloz <devel@ivanveloz.com>
2023-11-12 00:57:53 +0000

    Initial implementation of finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -13,3 +13,26 @@
 # Example invocation:
 
 #       finder.sh /tmp/aesd/assignment1 linux
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: finder.sh [filesdir] [searchstr]"
+    echo "Example invocation: finder.sh /tmp/aesd/assignment1 linux"
+    exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d $filesdir ]
+then
+    echo "${filesdir} is not a directory"
+    exit 1
+fi
+
+file_count=$(grep -r -l ${searchstr} ${filesdir} | wc -l)
+match_count=$(grep -r ${searchstr} ${filesdir} | wc -l)
+
+
+echo "The number of files are ${file_count} and the number of matching lines are ${match_count}"
+exit 0
