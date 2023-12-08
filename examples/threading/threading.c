7a2a51d23c693925c8e05d06480b0987636a977f
Ivan Veloz <devel@ivanveloz.com>
2023-12-08 17:05:58 -0500

    Partial start_thread_obtaining_mutex implementation

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
@@ -10,10 +11,17 @@
 
 void* threadfunc(void* thread_param)
 {
-
+    
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    useconds_t obtain_time = (useconds_t) thread_func_args->obtain_ms;     // warning: this is casting signed into unsigned
+    useconds_t release_time = (useconds_t) thread_func_args->release_ms;   // warning: this is casting signed into unsigned
+    if(usleep(100)) {
+        perror("threacfunc:usleep");
+    }
+    
     return thread_param;
 }
 
@@ -28,6 +36,21 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *td;
+    td = (struct thread_data *) malloc(sizeof(struct thread_data));
+
+    if (td == NULL) {
+        perror("start_thread_obtaining_mutex: malloc");
+        return false;
+    }
+
+    // TODO: setup mutex 
+    
+    // DONE: setup wait arguments
+    td->obtain_ms = wait_to_obtain_ms;
+    td->release_ms = wait_to_release_ms;
+
+    // TODO pass thread_data to created thread
+    
 }
 
