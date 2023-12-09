5f21de7fbafbc4ccd4d6f927ce755d59b21f9954
Ivan Veloz <devel@ivanveloz.com>
2023-12-09 13:22:50 -0500

    Changed inline functions to extern inline

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -141,7 +141,7 @@ int open_file(char *path) {
  * Returns
  *   zero on success. On error, -1 is returned and errno is set appropriately.
  */
-inline int close_file(int desc) {
+extern inline int close_file(int desc) {
     return (close(desc));
 }
 
@@ -155,7 +155,7 @@ inline int close_file(int desc) {
  *    -1  on unspecified error
  *   You may want to check errno for the last system call error.
  */
-inline int mk_dir(const char *path) {
+extern inline int mk_dir(const char *path) {
     return mkdir(path, MODE_MKDIR);
 }
 
