ec408da1ff7c4ff66744c2afd62d02c508081832
Ivan Veloz <devel@ivanveloz.com>
2023-12-10 20:50:46 -0500

    Implemented bind to socket and accept connection

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,8 +1,14 @@
 #define _POSIX_C_SOURCE 200809L
+#define __DEBUG_MESSAGES 1
+
+#include <stdbool.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <errno.h>
+#include <signal.h>
 #include <string.h>
 #include <syslog.h>
 
@@ -22,33 +28,109 @@
 
 int main(int argc, char *argv[]) {
     openlog("aesdsocket", LOG_CONS|LOG_PERROR|LOG_PID, LOG_USER);
-
     syslog(LOG_DEBUG, "server started");
-    int sd = opensocket();
-    if( sd == -1 ) {
+    int sfd = opensocket();
+    if( sfd == -1 ) {
         log_errno("main(): opensocket()");
         return 1;
     }
-
-    closesocket(sd);
+    socketfiledesc = sfd;
+    // start SIGINT and SIGTERM signal handlers here
+    acceptconnection(socketfiledesc);  // should be on its own thread
+    closesocket(socketfiledesc);
     syslog(LOG_DEBUG, "server stopped");
     closelog();
-    return 1;
+    return 0;
 }
 
 int opensocket() {
-    int sd = socket(AF_INET, SOCK_STREAM, 0);
-    if( sd == -1 ) {
-        log_errno("opensocket(): socket()");
+
+    int errnoshadow;
+
+    struct addrinfo *bindaddresses, *rp;
+    int sfd = -1;    //socket file descriptor
+    int r = getaddrinfo(aesd_netparams.ip, aesd_netparams.port,
+                        &(aesd_netparams.ai),&bindaddresses);
+    if (r) {
+        log_gai("opensocket(): getaddrinfo()", r);
         return -1;
     }
-    return sd;
+
+    // This for loop comes from the example at the getaddrinfo(3) manpage,
+    // on the Linux Programmer's Manual. Pretty interesting for loop honestly.
+    // This loop will iterate over the bindaddresses until one is found that
+    // can be used to open a socket. In my testing only one address is returned
+    // usually.
+    for(rp = bindaddresses; rp != NULL; rp = rp->ai_next) {
+        sfd = socket(   aesd_netparams.ai.ai_family,
+                        aesd_netparams.ai.ai_socktype,
+                        aesd_netparams.ai.ai_protocol);
+        if( sfd == -1 ) {
+            syslog(LOG_DEBUG, "opensocket(): socket() loop: %m");
+            continue;
+        }
+        if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0) {
+            break;
+        }
+        else {
+            log_errno("opensocket(): bind()");
+            goto errorcleanup;
+        }
+
+    }
+    if (rp == NULL) {
+        syslog(LOG_ERR, "opensocket(): could not bind to any address");
+        goto errorcleanup;
+    }
+    // the following type cast comes from https://beej.us/guide/bgnet/html/
+    void *addr = &(((struct sockaddr_in *)rp->ai_addr)->sin_addr);
+    char ipstr[INET_ADDRSTRLEN];
+    inet_ntop(rp->ai_family, addr, ipstr, sizeof(ipstr));
+    syslog(LOG_INFO, "Bound to %s", ipstr);
+    if(listen(sfd, aesd_netparams.backlog)) {
+        log_errno("opensocket(): listen()");
+        goto errorcleanup;
+    }
+    freeaddrinfo(bindaddresses);
+    return sfd;
+
+    errorcleanup:
+    errnoshadow = errno;
+    close(sfd);
+    freeaddrinfo(bindaddresses);
+    errno = errnoshadow;
+    return -1;
 }
 
-int closesocket(int sd) {
-    if ( close(sd) ) {
+int closesocket(int sfd) {
+    if ( close(sfd) ) {
         log_errno("closesocket(): close()");
         return -1;
+    }
+    return 0;
+}
+
+// The proper way to use this is as a thread, because it has a blocking 
+// function.
+int acceptconnection(int sfd) {
+    flag_accepting_connections = true;
+    while(flag_accepting_connections) {
+        int rsfd;   //receiving socket-file-descriptor
+        struct sockaddr_storage client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        rsfd = accept(sfd,(struct sockaddr *)&client_addr,&client_addr_len);
+
+        #ifdef __DEBUG_MESSAGES
+            char hoststr[NI_MAXHOST];
+            char portstr[NI_MAXSERV];
+            if (getnameinfo((struct sockaddr *)&client_addr, client_addr_len,
+                            hoststr, sizeof(hoststr), portstr, sizeof(portstr),
+                            NI_NUMERICHOST | NI_NUMERICSERV) == 0) {
+                syslog(LOG_DEBUG,"Incoming connection from %s port %s", hoststr, portstr);
+            }
+            syslog(LOG_DEBUG,"Opened new descriptor # %i",rsfd);
+        #endif
+        
     }
     return 0;
 }
@@ -57,4 +139,18 @@ void log_errno(const char *funcname) {
     int local_errno = errno;
     syslog(LOG_ERR, "%s: %m", funcname);
     errno = local_errno;
+}
+
+void log_gai(const char *funcname, int errcode) {
+    const char *errstr = gai_strerror(errcode);
+    syslog(LOG_ERR, "%s: %s", funcname, errstr);
+}
+
+// TODO setup sigaction
+void sigint_handler(int signo) {
+    switch(signo) {
+        case SIGINT:
+            if(flag_accepting_connections)
+                flag_accepting_connections = false;
+    }
 }
--- server/aesdsocket.h
+++ server/aesdsocket.h
@@ -1,15 +1,42 @@
+#  define NI_MAXHOST      1025  //these should come from netdb.h but I can't
+#  define NI_MAXSERV      32    //seem to get the ifdef to work right away
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 struct socket_params {
-    int server_port;
-    char *server_addr;
-
+    char *port;
+    char *ip;
+    int backlog;
+    struct addrinfo ai;
 };
 
-const struct socket_params aesdsocket_params  = {
-    .server_port = 9000,
-    .server_addr = "localhost",
+const struct socket_params aesd_netparams  = {
+    .port = "9000",
+    .ip = "0.0.0.0",                         // NULL will bind to all interfaces
+    .backlog = 100,
+    .ai = {
+        .ai_family = AF_INET,                // AF_UNSPEC for dual stack
+        .ai_socktype = SOCK_STREAM,           // | SOCK_NONBLOCK,
+        .ai_protocol = 0,
+        .ai_flags = AI_PASSIVE,
+        .ai_canonname = NULL,
+        .ai_addr = NULL,
+        .ai_addrlen = 0,
+        .ai_next = NULL
+    }
 };
+// To bind to a specific interface use .ip = "127.0.0.1" for example.
+
+int socketfiledesc = -1;
+
+bool flag_accepting_connections = false;
 
 int main(int argc, char *argv[]);
 int opensocket();
-int closesocket(int sd);
+int closesocket(int sfd);
+int acceptconnection(int sfd);
 void log_errno(const char *funcname);
+void log_gai(const char *funcname, int errcode);
+void sigint_handler(int signo);
